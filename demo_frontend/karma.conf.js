c5f22e5161c7a0aa8ed77e0c767a0d336f3618a5
Bi <jackson.shengpeng.bi@accenture.com>
2020-09-15 11:09:05 +0800

    add karma jasmine

--- demo_frontend/karma.conf.js
+++ demo_frontend/karma.conf.js
@@ -1,32 +1,14 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/AngularMaterialApp'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
+    config.set({
+        frameworks: ["jasmine", "karma-typescript"],
+        files: ["src/**/*.ts"// *.tsx for React Jsx
+        ],
+        preprocessors: {
+            "**/*.ts": "karma-typescript"// *.tsx for React Jsx
+        },
+        reporters: ["progress", "karma-typescript"],
+        browsers: ["Chrome"]
+    });
 };
+
+
